8bd6f6d4c41b9201f8ccb084978eb9150a8091ed
kohjp <rshnms12@gmail.com>
2023-07-03 19:54:50 +0900

    Feat: Add api vue practice

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,33 +1,25 @@
 <template>
   <div class="card shadow-sm">
-    <svg
-      class="bd-placeholder-img card-img-top"
-      width="100%"
-      height="225"
-      xmlns="http://www.w3.org/2000/svg"
-      role="img"
-      aria-label="Placeholder: Thumbnail"
-      preserveAspectRatio="xMidYMid slice"
-      focusable="false"
-    >
-      <title>Placeholder</title>
-      <rect width="100%" height="100%" fill="#55595c" />
-      <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-    </svg>
+    <span class="img" :style="{ backgroundImage: `url(${item.imgPath})` }" />
     <div class="card-body">
       <p class="card-text">
-        {{ item }}
+        {{ item.name }} &nbsp;
+        <small class="discount badge bg-danger">{{ item.discountPer }}%</small>
       </p>
       <div class="d-flex justify-content-between align-items-center">
-        <div class="btn-group">
-          <button type="button" class="btn btn-sm btn-outline-secondary">
-            View
-          </button>
-          <button type="button" class="btn btn-sm btn-outline-secondary">
-            Edit
-          </button>
-        </div>
-        <small class="text-body-secondary">9 mins</small>
+        <button class="btn btn-primary">구입하기</button>
+        <small class="price text-body-secondary"
+          >{{
+            item.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+          }}원</small
+        >
+        <small class="real text-danger"
+          >{{
+            (item.price - (item.price * item.discountPer) / 100)
+              .toString()
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+          }}원</small
+        >
       </div>
     </div>
   </div>
@@ -37,9 +29,22 @@
 export default {
   name: "Card",
   props: {
-    item: String,
+    item: Object,
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.card .img {
+  width: 100%;
+  display: inline-block;
+  height: 250px;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+.card .card-body .price {
+  text-decoration: line-through;
+}
+</style>
